c976195d510f30500f1c6d5efb1031f58b43a832
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2016-10-24 21:37:53 +0100

    Finished basic functionality, added doc comments

--- index.ts
+++ index.ts
@@ -1,7 +1,18 @@
-import { ValidatorFn, AsyncValidatorFn, FormControl } from '@angular/forms';
+import { ValidatorFn, AsyncValidatorFn, FormControl, AbstractControl } from '@angular/forms';
+
+// Copy-pasting quick hacks from Angular 2 sources, lol.
+declare var Reflect: any;
 
 const ControlSymbol: string = 'FDControl';
 
+/**
+ * Class property decorator for specifying FormControl constructor arguments to be used with
+ * Angular forms.
+ *
+ * @param definition
+ * @returns {(model:any, key:string)=>void}
+ * @constructor
+ */
 export function FDControl(definition: ControlDefinition) {
     return function (model: any, key: string): void {
         let meta: MetaDefinition = Reflect.getMetadata(ControlSymbol, model.constructor) || {};
@@ -10,8 +21,14 @@ export function FDControl(definition: ControlDefinition) {
     }
 }
 
-export function fromModel(model: any) {
-    let result = {};
+/**
+ * Generates form fields definition from specified model object.
+ *
+ * @param model
+ * @returns {{}}
+ */
+export function fromModel(model: any): { [key: string]: AbstractControl } {
+    let result: { [key: string]: AbstractControl } = {};
     let meta: MetaDefinition = Reflect.getMetadata(ControlSymbol, model.constructor);
 
     for (let key in meta) {
@@ -29,10 +46,15 @@ export function fromModel(model: any) {
     return result;
 }
 
+/**
+ * FDControl definition interface.
+ */
 export interface ControlDefinition {
+    /** Overrides property name. */
     name?: string;
     validators?: ValidatorFn|ValidatorFn[];
     asyncValidators?: AsyncValidatorFn|AsyncValidatorFn[];
+    /** Overrides model objects value. */
     overrideValue?: any;
 }
 
